a3b48fba56cce335d641d838226a3181d8178aaa
mostafa-hiebah <mhiebah@gmail.com>
2025-01-27 12:54:32 +0200

    add run playbook

--- terraform/9-key_pair.tf
+++ terraform/9-key_pair.tf
@@ -7,7 +7,7 @@ resource "tls_private_key" "rsa_key_pair" {
 
 # extract public key
 resource "aws_key_pair" "tf_public_key_pair" {
-  key_name   = "tf_key_pair"
+  key_name   = "tf_key_pair2"
   public_key = tls_private_key.rsa_key_pair.public_key_openssh
 }
 
@@ -15,5 +15,5 @@ resource "aws_key_pair" "tf_public_key_pair" {
 # store private key into my machine to use it with ssh
 resource "local_file" "tf_local_file" {
   content  = tls_private_key.rsa_key_pair.private_key_pem
-  filename = "tf_key_pair.pem"
+  filename = "tf_key_pair2.pem"
 }
